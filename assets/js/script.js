f3b57a4c561405e3c3eee8ef870b96d616d44aed
Nunder <nathannunderwood@gmail.com>
2022-07-27 15:42:14 -0700

    Basic HTML structure built. Need to figure out how to prompt questions for user to answer

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var startQuiz = document.querySelector('#start');
+
+
+startQuiz.addEventListener('click', function() {
+    // may need to add event parameter to function to prevent default (event.preventdefault()) so form doesn't clear. we'll see
+
+    
+})
+
+
