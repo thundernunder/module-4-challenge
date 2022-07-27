526c1423bacebe60872f0a85524cb988adfe3c47
Nunder <nathannunderwood@gmail.com>
2022-07-27 16:03:55 -0700

    Figure out how to make beginning page section disappear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
 var startQuiz = document.querySelector('#start');
+var startContainer = document.querySelector('#start-container');
+
+function showQuestions() {
+
+}
 
 
-startQuiz.addEventListener('click', function() {
+startQuiz.addEventListener('click', function(event) {
     // may need to add event parameter to function to prevent default (event.preventdefault()) so form doesn't clear. we'll see
+    var element = event.target;
 
-    
+    if (element.matches("#start-container")) {
+        var state = element.getAttribute('data-state');
+    }
+        if (state === "visible") {
+            element.dataset.state = "hidden";
+        }
 })
+
+
 
 
