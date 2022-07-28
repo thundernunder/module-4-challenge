0bef81c9c94161917c2fff21424def7b71f97da9
Nunder <nathannunderwood@gmail.com>
2022-07-27 20:49:30 -0700

    still working on hidden data-state

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,20 @@
-var startQuiz = document.querySelector('#start');
+var startButton = document.querySelector("#start");
 var startContainer = document.querySelector('#start-container');
 
-function showQuestions() {
 
-}
-
-
-startQuiz.addEventListener('click', function(event) {
+startContainer.addEventListener('click', function(event) {
     // may need to add event parameter to function to prevent default (event.preventdefault()) so form doesn't clear. we'll see
     var element = event.target;
 
     if (element.matches("#start-container")) {
         var state = element.getAttribute('data-state');
-    }
+
         if (state === "visible") {
-            element.dataset.state = "hidden";
+            element.setAttribute("data-state", "hidden");
+        } else {
+            element.dataset.state = "visible";
         }
+    }
 })
 
 
