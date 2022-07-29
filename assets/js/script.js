6404c764995e0c6306d60a3b7cb4836ef05e9434
Nunder <nathannunderwood@gmail.com>
2022-07-28 22:44:37 -0700

    Figured out appear and disappear. Need to build rest of questions, set timer, and calculate quiz score

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,52 @@
 var startButton = document.querySelector("#start");
 var startContainer = document.querySelector('#start-container');
+var firstQuestion = document.querySelector("#question-one");
+var firstParagraph = document.querySelector("#q-1-p");
+
+var score = document.querySelector('#view-score');
+var timer;
 
 
-startContainer.addEventListener('click', function(event) {
-    // may need to add event parameter to function to prevent default (event.preventdefault()) so form doesn't clear. we'll see
-    var element = event.target;
 
-    if (element.matches("#start-container")) {
-        var state = element.getAttribute('data-state');
+var q1 = {
+    question: "What are the three foundational languages of web development?",
+    choices: ["A. python, c++, javascript", "B. html, css, javascript", "C. html, css, python"], 
+    answer: "B. html, css, javascript"
+}
 
-        if (state === "visible") {
-            element.setAttribute("data-state", "hidden");
-        } else {
-            element.dataset.state = "visible";
-        }
+var q2 = {
+    question: "Which language is primarily used to style webpage elements?", 
+    choices: ["A. javascript", "B. python", "C. css"], 
+    answer: "C. css"
+}
+
+var q3 = {
+    question: "What is the best coding language?", 
+    choices: ["A. python", "B. css", "C. not sure"], 
+    answer: "C. not sure"
+}
+    
+
+       
+
+
+   
+
+startButton.addEventListener('click', function start() {
+    if (startContainer.dataset.state === 'visible') {
+        startContainer.style.display = 'none';
     }
-})
+    questionOne();
+}) 
+
+function questionOne () {
+    // timer start will go here
+    firstQuestion.style.display = 'block';
+
+    firstParagraph.textContent = q1.question;
+}
+
+
 
 
 
